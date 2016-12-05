6440000facf9d7922fa7ad2cf8b1126a575ed0d3
Joshua <joshuawcleveland@gmail.com>
2016-12-05 00:16:29 -0800

    Properly commented the code.

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+/*
+This program generates a picture and covers it with tiles.
+The user can mouse over the tiles to remove them,
+revealing the picture beneath. I threw in some fun with the Troll.
+*/
+
 $(document).ready(function(){
 var score = 0;
   var createGrid = function(rows) {
 
+    /* Need to remove all objects that may have stuck around before
+    drawing our new ones.*/
     $('td.squares').css('background-color','white');
     $('td.squares').remove();
     $('tr').remove();
     $('p').remove();
     $('img').remove();
 
+    /* We generate the grid by using nested 'for' loops.
+    The first loop draws a series of rows, and the nested loop
+    creates a relevant number of table data objects for each of them. */
     for (var i = 0; i < rows; i++) {
       var $tr = $('<tr id="cell' + i + '" class="rows"></tr>');
       $('#myTable').append($tr);
@@ -18,6 +29,7 @@ var score = 0;
       }
     }
 
+    // Choose a random image.
     var randomSrc="";
     var randomizer = Math.floor(Math.random() * 3);
     if (randomizer === 0) {
@@ -27,14 +39,16 @@ var score = 0;
     } else if (randomizer === 2) {
       randomSrc = "images/troll.jpg";
     }
+    // Set that random image.
     $("#myTable").css('background-image','url("' + randomSrc + '")');
 
+    // Do various fun things based on what the image ends up being.
     if (randomizer === 0) {
       $('div.squares').css('background-color','#078407')
       $('div.squares').css('outline','solid #078407 2px')
       $('div.squares').hover(function(){
         $(this).remove();
-        score += 10;
+        score += 10;           // Can't forget to increment our score!
       });
     } else if (randomizer === 1) {
       $('div.squares').css('background-color','#0044cc')
@@ -46,11 +60,14 @@ var score = 0;
     } else if (randomizer === 2) {
 
         $('div.squares').hover(function(){
+          /* Everytime a user mouses over a tile, run this test */
           var randomizerTROLL = Math.floor(Math.random() * 2);
           if (randomizerTROLL === 0) {
+            // Option 1: 50% chance to remove the tile
             $(this).remove()
             score += 10;
           } else if (randomizerTROLL === 1) {
+            // Option 2: 50% chance to give the tile a random color
             var ranRGB = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
             $(this).css('background-color', ranRGB);
             $(this).css('outline','solid ' + ranRGB + ' 2px');
@@ -59,6 +76,8 @@ var score = 0;
     }
   }
 
+  /* When someone clicks our button, draw a grid based on their input
+  also create, or update, our Score. */
   $('button').click(function(){
     gridSquares = prompt("Input a number between 1 and 10","1 - 10");
     if (gridSquares > 10) {
@@ -66,6 +85,7 @@ var score = 0;
     } else if(gridSquares < 1){
       gridSquares = 1;
     }
+    // Score: 
     createGrid(gridSquares);
     var $p = $("<p>Score: " + score + "</p>");
     $('#paragraph').append($p)
